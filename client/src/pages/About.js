6b7e7b517db9b2defaf1664f7f7e0a5990e1cc5f
Ramtin Abolfazli <Ramtina1381@gmail.com>
2025-07-21 22:00:09 -0400

    Full alignment on Boxes

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -4,16 +4,11 @@ import './Pages.css';
 const About = () => {
   return (
     <div className="about-container">
-
-
-      
-    
-
       {/* Mission Section */}
       <section className="mission-section">
         <div className="section-container">
-                  <div className="centered-heading-container">
-          <h1>About Us</h1>
+          <div className="centered-heading-container">
+            <h1>About Us</h1>
         </div>
           <h2 className="section-title">Our Mission</h2>
           <div className="mission-content">
