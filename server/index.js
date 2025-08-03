7f117639225bfe32e8845c32936786eb7251e50d
Ramtin Abolfazli <Ramtina1381@gmail.com>
2025-08-03 11:37:32 -0400

    sendgrid updated

--- server/index.js
+++ server/index.js
@@ -47,13 +47,20 @@ app.post("/api/contact", async (req, res) => {
   // Email configuration
 const transporter = nodemailer.createTransport({
   host: 'smtp.sendgrid.net',
-  port: 587,
+  port: 465,
+  secure: true,
   auth: {
     user: 'apikey',
     pass: process.env.SENDGRID_API_KEY
   }
 });
-
+transporter.verify(function(error, success) {
+  if (error) {
+    console.log('SMTP connection error:', error);
+  } else {
+    console.log('Server is ready to take our messages');
+  }
+});
 
   const mailOptions = {
     from: process.env.EMAIL_USER, // Use your email as sender
